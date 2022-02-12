3cb7bb47a7aa04944dff0eee0dbd01cf45fe4ae5
Anu Lama <anulama@u.boisestate.edu>
2022-02-12 13:37:41 -0700

    API routes setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// app.get('/api/goals', (req, res) => {
+//   res.status(200).json({ message: 'Get Goals' });
+//   //pass json object or array inside the function
+// });
+
+app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
