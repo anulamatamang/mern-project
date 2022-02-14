b4c158b46bd9d704f80e46dd11f8f2b44a44e593
Anu Lama <anulama@u.boisestate.edu>
2022-02-14 00:00:07 -0700

    authentication and authorization implementation

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -7,6 +7,7 @@
 const asyncHandler = require('express-async-handler');
 
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 //and then all we have to do is wrap the functions with asyncHandler
 
@@ -52,6 +53,19 @@ const updateGoal = asyncHandler(async (req, res) => {
     throw new Error('Goal not found');
   }
 
+  const user = await User.findById(req.user.id);
+
+  //Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  //make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
   const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
@@ -68,6 +82,21 @@ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('Goal not found');
   }
+
+  const user = await User.findById(req.user.id);
+
+  //Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  //make sure the logged in user matches the goal user
+  if (goal.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
+
   await goal.remove();
   res.status(200).json({ id: req.params.id });
 });
