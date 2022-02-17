6c19a56e191ce4ac0d6f44f01c6be43e0117ff15
Anu Lama <anulama@u.boisestate.edu>
2022-02-16 20:04:05 -0700

    implemented login in frontend

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,11 @@
 
-
-
 import { useState, useEffect } from 'react';
 import { FaSignInAlt, FaSingInAlt } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { login, reset} from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 
 function Login() {
@@ -15,6 +18,23 @@ function Login() {
 
   const {email, password } = formData;
 
+   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth);
+
+    useEffect(() => {
+    if(isError){
+      toast.error(message);
+    }
+
+    if(isSuccess || user) {
+      navigate('/');
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   const onChange = (e) => {
     setFormData((prevState) => ({
       ...prevState,
@@ -24,8 +44,18 @@ function Login() {
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const userData = {
+      email,
+      password
+    }
+
+    dispatch(login(userData));
   }
 
+   if(isLoading) {
+    return <Spinner />
+  }
   return (
     <>
       <section className="heading">
