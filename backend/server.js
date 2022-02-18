33c3b0c2416faaa86aa924d1cdeeff0b67282ca5
Anu Lama <anulama@u.boisestate.edu>
2022-02-17 23:53:41 -0700

    Served frontend in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
+const res = require('express/lib/response');
 
 connectDB();
 
@@ -19,6 +21,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+//Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  //As far as our route, for any route aside from the API routes above, load the index.html file that is in static build folder
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => {
